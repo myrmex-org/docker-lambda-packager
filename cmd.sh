9de1687adc7d5ce26e3f0407c63150b7855114a6
AlexisNo <alexis@henaut.net>
2017-07-04 18:17:27 -0300

    fix bash conditions

--- cmd.sh
+++ cmd.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-if [ "$RUNTIME" = "python" or "$RUNTIME" = "python2" ]; then
+if [ "$RUNTIME" == "python" ] || [ "$RUNTIME" == "python2" ]; then
     su $DEFAULT_USER -c "pip install --requirement requirements.txt --upgrade --target ."
-elif [ "$RUNTIME" = "python3" ]; then
+elif [ "$RUNTIME" == "python3" ]; then
     su $DEFAULT_USER -c "pip3 install --requirement requirements.txt --upgrade --target ."
 else
     su $DEFAULT_USER -c "npm install --production"
